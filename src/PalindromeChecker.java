3685677dafc9155df6904cd59569d9d516f9b82c
rohanrpais <rohanrpais@gmail.com>
2026-02-19 10:41:38 +0530

    UC2 solved

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -5,5 +5,21 @@ public class PalindromeChecker {
         System.out.println("Version: 1.0 ");
         System.out.println("System Initialised Successfully");
         //test case 1
+        /** Use case 2 : Hardcoded Palindrome checker*/
+        String input ="madam";
+        boolean palindrome=true;
+        input =input.toLowerCase();
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                palindrome = false;
+                break;
+            }
+        }
+        if(palindrome){
+            System.out.println("It is a Palindrome");
+        }
+        else{
+            System.out.println("It is not a Palindrome");
+        }
     }
 }
