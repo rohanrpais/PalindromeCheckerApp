e4587b147c2f25c234a42c5bc8b9f42cda9bc3f3
rohanrpais <rohanrpais@gmail.com>
2026-02-26 11:27:14 +0530

    Update

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class PalindromeChecker {
     public static void main(String[] args) {
 /** Use case 8: Linked List Based Palindrome **/
-        String input = "level";
+        String input = "nigga";
         input = input.toLowerCase();
         char[] arr = input.toCharArray();
         boolean isPalindrome = true;
@@ -10,12 +10,24 @@ public class PalindromeChecker {
         for( char c : arr){
             linkedlist.add(c);
         }
-            while(linkedlist.size()>1){
-                if(linkedlist.removeLast()!=linkedlist.removeFirst()){
-                    isPalindrome=false;
+        System.out.println(linkedlist);
+        LinkedList<Character> reverselinkedlist =new LinkedList<>();
+        if(arr.length%2 ==0){
+            for (int i=0;i<=linkedlist.size()/2; i++){
+                if (linkedlist.get(i) != linkedlist.get(linkedlist.size() - 1 - i)) {
+                    isPalindrome = false;
                     break;
-          }
+                }
             }
+        }
+        else {
+            for (int i=0;i < linkedlist.size() / 2; i++){
+                if (linkedlist.get(i) != linkedlist.get(linkedlist.size() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+        }
         if(isPalindrome){
             System.out.println("it is a palindrome");}
             else{
