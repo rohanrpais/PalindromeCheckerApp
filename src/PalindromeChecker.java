14f2a11d9ed3343f40fdcf4c63752575fa58f6a6
rohanrpais <rohanrpais@gmail.com>
2026-02-26 10:11:42 +0530

    Implemented Dequeue based Palindrome1

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,18 +1,17 @@
 import java.util.*;
 public class PalindromeChecker {
     public static void main(String[] args) {
-/** Use case 6: Queue and Stack Based Palindrome **/
-        String input = "keshav";
+/** Use case 7: Deque Based Palindrome **/
+        String input = "refer";
         input = input.toLowerCase();
+        char[] arr = input.toCharArray();
         boolean isPalindrome = true;
-        Queue<Character> queue= new LinkedList<>();
-        Stack<Character> stack=new Stack<>();
-        for( char c : input.toCharArray()){
-            queue.add(c);
-            stack.push(c);
+        Deque<Character> deque =new ArrayDeque<>();
+        for( char c : arr){
+            deque.add(c);
         }
-            while(!queue.isEmpty()&& !stack.isEmpty()){
-                if(stack.pop()!=queue.remove()){
+            while(deque.size()>1){
+                if(deque.removeLast()!=deque.removeFirst()){
                     isPalindrome=false;
                     break;
           }
