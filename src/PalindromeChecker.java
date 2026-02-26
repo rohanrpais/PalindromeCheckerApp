5b0738012464e1dc8c763a2eebdbcb24714ae1e4
Rohan Pais <rohanrpais@gmail.com>
2026-02-26 08:46:54 +0530

    Update PalindromeChecker.java

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,10 +1,5 @@
 public class PalindromeChecker {
-/** Use case 1 : Application entry and Welcome Message **/
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version: 1.0 ");
-        System.out.println("System Initialised Successfully");
-        //test case 1
         /** Use case 2 : Hardcoded Palindrome checker*/
         String input ="madam";
         boolean palindrome=true;
