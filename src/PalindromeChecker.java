347de99006ec8d4d2b9494335f73b80d4b850ca3
rohanrpais <rohanrpais@gmail.com>
2026-03-06 08:52:14 +0530

    UC9: Recursive Palindrome Check

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,36 +1,15 @@
 import java.util.*;
 public class PalindromeChecker {
-    public static void main(String[] args) {
-/** Use case 8: Linked List Based Palindrome **/
-        String input = "nigga";
-        input = input.toLowerCase();
-        char[] arr = input.toCharArray();
-        boolean isPalindrome = true;
-        LinkedList<Character> linkedlist =new LinkedList<>();
-        for( char c : arr){
-            linkedlist.add(c);
-        }
-        System.out.println(linkedlist);
-        LinkedList<Character> reverselinkedlist =new LinkedList<>();
-        if(arr.length%2 ==0){
-            for (int i=0;i<=linkedlist.size()/2; i++){
-                if (linkedlist.get(i) != linkedlist.get(linkedlist.size() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
+        public static boolean isPalindrome(String s) {
+            if (s.length() <= 1) {
+                return true;
             }
-        }
-        else {
-            for (int i=0;i < linkedlist.size() / 2; i++){
-                if (linkedlist.get(i) != linkedlist.get(linkedlist.size() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
+            if (s.charAt(0) != s.charAt(s.length() - 1)) {
+                return false;
             }
+            return isPalindrome(s.substring(1, s.length() - 1));
         }
-        if(isPalindrome){
-            System.out.println("it is a palindrome");}
-            else{
-                System.out.println("it is not a palindrome");
-            }
-    }}
+        public static void main(String[] args) {
+            System.out.println(isPalindrome("madam"));
+        }
+    }
