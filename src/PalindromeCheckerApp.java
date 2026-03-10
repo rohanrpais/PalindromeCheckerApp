b67401f5c58e0c06b2b744b2e21349b5ba0fc498
Rohan <rohanrpais@gmail.com>
2026-03-11 00:16:16 +0530

    UC12 implemented

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        PalindromeStrategy strategy = new StackStrategy();
+        boolean isPalindrome = strategy.check(normalized);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        scanner.close();
+    }
+}
